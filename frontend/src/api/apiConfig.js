418e07c33f6b4bb8e52c35bc717a3797f7a0e06c
faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2025-06-25 12:53:41 +0700

    baseurl

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -2,7 +2,7 @@ const API_CONFIG = {
     // URL dasar untuk backend API Anda
     // Contoh: 'http://147.139.243.222:3000/v1' untuk development
     // Contoh: 'https://api.yourdomain.com/v1' untuk production
-    API_BASE_URL: 'https://examwoi-10e26c93442d.herokuapp.com/v1',
+    API_BASE_URL: 'https://examwoi-10e26c93442d.herokuapp.com/',
 
     // Kunci API untuk otentikasi aplikasi ke server
     // Harus sama dengan API_KEY yang dikonfigurasi di backend Anda.
